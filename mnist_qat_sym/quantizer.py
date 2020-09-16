919fc7ff556c61f610ebcccd883aaa7cd7f51e19
RobertLuobo <1034016780@qq.com>
2020-09-17 01:41:55 +0800

    Add note

--- mnist_qat_sym/quantizer.py
+++ mnist_qat_sym/quantizer.py
@@ -4,6 +4,10 @@ from collections import namedtuple
 from config import cfg
 
 QTensor = namedtuple('QTensor', ['tensor', 'scale', 'zero_point'])
+'''
+Following functions for symmetric Mapping-[-128 ~ +127] 
+'''
+
 
 def calcScaleZeroPointSym(min_val, max_val, num_bits=8):
     # Calc Scale
@@ -17,8 +21,9 @@ def calcScaleZeroPointSym(min_val, max_val, num_bits=8):
 
 n_count = 0
 input_n_count = 1
-def quantize_tensor_input_sym(x, num_bits=8, min_val=None, max_val=None):
 
+
+def quantize_tensor_input_sym(x, num_bits=8, min_val=None, max_val=None):
     if not min_val and not max_val:
         min_val, max_val = x.min(), x.max()
 
@@ -26,8 +31,8 @@ def quantize_tensor_input_sym(x, num_bits=8, min_val=None, max_val=None):
     qmax = 2. ** (num_bits - 1) - 1.
 
     scale, zero_point = calcScaleZeroPointSym(min_val, max_val, num_bits)
-    q_x =  x / scale
-    q_x.clamp_(-qmax-1, qmax).round_()
+    q_x = x / scale
+    q_x.clamp_(-qmax - 1, qmax).round_()
     q_x = q_x.round()
 
     global input_n_count
@@ -40,8 +45,9 @@ def quantize_tensor_input_sym(x, num_bits=8, min_val=None, max_val=None):
 
 
 weight_n_count = 0
-def quantize_tensor_weight_sym(x, num_bits=8, min_val=None, max_val=None):
 
+
+def quantize_tensor_weight_sym(x, num_bits=8, min_val=None, max_val=None):
     if not min_val and not max_val:
         min_val, max_val = x.min(), x.max()
 
@@ -49,8 +55,8 @@ def quantize_tensor_weight_sym(x, num_bits=8, min_val=None, max_val=None):
     qmax = 2. ** (num_bits - 1) - 1.
 
     scale, zero_point = calcScaleZeroPointSym(min_val, max_val, num_bits)
-    q_x =  x / scale
-    q_x.clamp_(-qmax-1, qmax).round_()
+    q_x = x / scale
+    q_x.clamp_(-qmax - 1, qmax).round_()
     q_x = q_x.round()
 
     global weight_n_count
@@ -61,6 +67,7 @@ def quantize_tensor_weight_sym(x, num_bits=8, min_val=None, max_val=None):
 
     return QTensor(tensor=q_x, scale=scale, zero_point=0)
 
+
 def dequantize_tensor_sym(q_x):
     return q_x.scale * (q_x.tensor.float())
 
@@ -68,38 +75,48 @@ def dequantize_tensor_sym(q_x):
 Conv_count = 0
 fc_count = 0
 
+'''
+Dong Xin paper uniform quantizer method: alpha * {0 , 1/127, -1/127, .... +1, -1 } by APoT
+formula:   
+\mathcal{Q}^{u}(\alpha, b)=\alpha \times\left\{0, \frac{\pm 1}{2^{b-1}-1}, \frac{\pm 2}{2^{b-1}-1}, \frac{\pm 3}{2^{b-1}-1}, \ldots,\pm 1\right\}
+'''
+
+
 class Qconv2d_INT(torch.nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
                  padding=0, dilation=1, groups=1, bias=True):
         torch.nn.Conv2d.__init__(self, in_channels, out_channels, kernel_size, stride=stride,
-                 padding=padding, dilation=dilation, groups=groups, bias=bias)
-
+                                 padding=padding, dilation=dilation, groups=groups, bias=bias)
 
     def conv2d_forward(self, input, num_bits=8):
+        # step1 : discrete
         min_val = torch.min(input)
         max_val = torch.max(input)
         q_input = quantize_tensor_input_sym(input, num_bits=num_bits, min_val=min_val, max_val=max_val)
 
         q_weight = quantize_tensor_weight_sym(self.weight, num_bits=num_bits,
-                                   min_val=torch.min(self.weight), max_val=torch.max(self.weight))
+                                              min_val=torch.min(self.weight), max_val=torch.max(self.weight))
 
         output = torch.nn.functional.conv2d(q_input.tensor.type(torch.IntTensor),
                                             q_weight.tensor.type(torch.IntTensor),
                                             self.bias, self.stride, self.padding, self.dilation, self.groups)
 
+        # step2 : find out alpha
+        input_alpha = (input.max() - input.min())
+        weight_alpha = (self.weight.max() - self.weight.min())
+
         global Conv_count
         Conv_count += 1
-        input_alpha = (input.max() - input.min()) / 2
-        weight_alpha = (self.weight.max() - self.weight.min()) / 2
         cfg.writer.add_scalars("Conv_input_alpha", {"Conv_input_alpha": input_alpha}, Conv_count)
         cfg.writer.add_scalars("Conv_weight_alpha", {"Conv_weight_alpha": input_alpha}, Conv_count)
-        decimal_point = 2 ** num_bits - 1
-        output = output / (decimal_point ** 2) * input_alpha * weight_alpha
-        # print(output.size())
-        # print(output == input)
-        Qoutput = QTensor(tensor=output, scale=q_weight.scale,zero_point=(q_weight.zero_point))
 
-        output = dequantize_tensor_sym(Qoutput)
+        # Step3 : calibration, add decimal point
+        decimal_point = 2 ** (num_bits - 1) - 1
+        output = output / (decimal_point ** 2) * input_alpha * weight_alpha
+
+        # Step4 :  dequantizer
+        Qoutput = QTensor(tensor=output, scale=q_weight.scale, zero_point=(q_weight.zero_point))
+        output = dequantize_tensor(Qoutput)
 
         return output
 
@@ -107,45 +124,52 @@ class Qconv2d_INT(torch.nn.Conv2d):
         return self.conv2d_forward(input)
 
 
+'''
+Dong Xin paper uniform quantizer method: alpha * {0 , 1/127, -1/127, .... +1, -1 } by APoT
+'''
+
 
 class QLinear_INT(torch.nn.Linear):
     def __init__(self, in_features, out_features, bias=True):
         torch.nn.Linear.__init__(self, in_features, out_features, bias=bias)
 
     def _linear_forward(self, input, num_bits=8, min_val=None, max_val=None):
-
+        # step1 : discrete
         q_input = quantize_tensor_input_sym(input, num_bits=num_bits,
-                                  min_val=torch.min(input), max_val=torch.max(input))
+                                            min_val=torch.min(input), max_val=torch.max(input))
 
         q_weight = quantize_tensor_weight_sym(self.weight, num_bits=num_bits,
-                                   min_val=torch.min(self.weight), max_val=torch.max(self.weight))
+                                              min_val=torch.min(self.weight), max_val=torch.max(self.weight))
 
         output = torch.nn.functional.linear(q_input.tensor.type(torch.IntTensor),
                                             q_weight.tensor.type(torch.IntTensor),
                                             self.bias)
+        # step2 : find out alpha
+        input_alpha = (input.max() - input.min())
+        weight_alpha = (self.weight.max() - self.weight.min())
 
-        global fc_count
-        fc_count += 1
-        input_alpha = (input.max() - input.min()) / 2
-        weight_alpha = (self.weight.max() - self.weight.min()) / 2
+        global Conv_count
+        Conv_count += 1
+        cfg.writer.add_scalars("Conv_input_alpha", {"Conv_input_alpha": input_alpha}, Conv_count)
+        cfg.writer.add_scalars("Conv_weight_alpha", {"Conv_weight_alpha": input_alpha}, Conv_count)
 
-        cfg.writer.add_scalars("fc_input_alpha", {"fc_input_alpha": input_alpha}, fc_count)
-        cfg.writer.add_scalars("fc_weight_alpha", {"fc_weight_alpha": input_alpha}, fc_count)
-
-        decimal_point = 2 ** num_bits - 1
+        # Step3 : calibration, add decimal point
+        decimal_point = 2 ** (num_bits - 1) - 1
         output = output / (decimal_point ** 2) * input_alpha * weight_alpha
 
-        Qoutput = QTensor(tensor=output, scale=q_weight.scale,zero_point=(q_weight.zero_point))
-        output = dequantize_tensor_sym(Qoutput)
+        # Step4 :  dequantizer
+        Qoutput = QTensor(tensor=output, scale=q_weight.scale, zero_point=(q_weight.zero_point))
+        output = dequantize_tensor(Qoutput)
 
         return output
-
 
     def forward(self, input):
         return self._linear_forward(input)
 
 
-
+'''
+Following functions for Asymmetric Mapping-[0-255] 
+'''
 def calcScaleZeroPoint(min_val, max_val, num_bits=8):
     # Calc Scale and zero point of next
     qmin = 0.
@@ -169,7 +193,6 @@ def calcScaleZeroPoint(min_val, max_val, num_bits=8):
 
 
 def quantize_tensor(x, num_bits=8, min_val=None, max_val=None):
-
     if not min_val and not max_val:
         min_val, max_val = x.min(), x.max()
 
@@ -193,6 +216,7 @@ def quantize_tensor(x, num_bits=8, min_val=None, max_val=None):
 def dequantize_tensor(q_x):
     return q_x.scale * (q_x.tensor.float() - q_x.zero_point)
 
+
 class FakeQuantOp(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x, num_bits=8, min_val=None, max_val=None):
@@ -201,7 +225,7 @@ class FakeQuantOp(torch.autograd.Function):
         return x
 
     @staticmethod
-    def backward(ctx, grad_output,  num_bits=8, min_val=None, max_val=None):
+    def backward(ctx, grad_output, num_bits=8, min_val=None, max_val=None):
         # x = grad_output
         # x = quantize_tensor(x, num_bits=num_bits, min_val=min_val, max_val=max_val)
         # x = dequantize_tensor(x)
@@ -210,15 +234,19 @@ class FakeQuantOp(torch.autograd.Function):
         return grad_output
 
 
+'''
+Fake quantization conv2d
+'''
 class Qconv2d(torch.nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
                  padding=0, dilation=1, groups=1, bias=True):
         torch.nn.Conv2d.__init__(self, in_channels, out_channels, kernel_size, stride=stride,
-                 padding=padding, dilation=dilation, groups=groups, bias=bias)
+                                 padding=padding, dilation=dilation, groups=groups, bias=bias)
 
     def conv2d_forward(self, input, num_bits=8, min_val=None, max_val=None):
-        output = torch.nn.functional.conv2d(FakeQuantOp.apply(input), FakeQuantOp.apply(self.weight), self.bias, self.stride, self.padding,
-                                          self.dilation, self.groups)
+        output = torch.nn.functional.conv2d(FakeQuantOp.apply(input), FakeQuantOp.apply(self.weight), self.bias,
+                                            self.stride, self.padding,
+                                            self.dilation, self.groups)
         # print(output)
 
         return output
@@ -227,6 +255,9 @@ class Qconv2d(torch.nn.Conv2d):
         return self.conv2d_forward(input)
 
 
+'''
+Fake quantization Linear
+'''
 class QLinear(torch.nn.Linear):
     def __init__(self, in_features, out_features, bias=True):
         torch.nn.Linear.__init__(self, in_features, out_features, bias=bias)
